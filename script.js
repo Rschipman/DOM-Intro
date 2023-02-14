340d9d07626a116de8a62feb7fb16625615b67f2
RChipman <rschipman@gmail.com>
2023-02-14 15:31:23 -0300

    more learning

--- script.js
+++ script.js
@@ -3,8 +3,9 @@
 // for (let img of allImages) {
 //   console.log(img.src);
 // }
-const links = document.querySelectorAll("p a");
+// const links = document.querySelectorAll("p a");
 
-for (let link of links) {
-  console.log(link.href);
-}
+// for (let link of links) {
+//   console.log(link.href);
+// }
+// document.querySelector("h1").innerHTML = "<i>Fluffy Chickens</i>";
